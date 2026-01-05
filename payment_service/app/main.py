f46158ada45e1afa2d6f0e6001759cd33f4ac7c1
The Great Kamikaze <kamikazeweb3@gmail.com>
2026-01-05 22:33:26 +0100

    please

--- payment_service/app/main.py
+++ payment_service/app/main.py
@@ -2,8 +2,13 @@ from fastapi import FastAPI
 from fastapi import APIRouter
 from payment_service.app.api.routes import paystack, stripe, webhooks
 from payment_service.app.api.routes.paystack import router as payment_router
+from payment_service.app.db.base import Base
+from payment_service.app.db.session import engine
+
 
 app = FastAPI(title="Payment Service", version="1.0.0")
+
+Base.metadata.create_all(bind=engine)
 
 app.include_router(paystack.router)
 app.include_router(stripe.router)
