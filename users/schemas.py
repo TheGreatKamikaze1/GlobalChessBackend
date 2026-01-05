27de488be91be4d3f2905f56ecd3cf65119fa297
The Great Kamikaze <kamikazeweb3@gmail.com>
2026-01-05 14:56:14 +0100

    PLEASE

--- users/schemas.py
+++ users/schemas.py
@@ -1,10 +1,10 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field
 
 class RegisterSchema(BaseModel):
     email: EmailStr
-    username: str
-    displayName: str
-    password: str
+    username: str = Field(..., max_length=30)
+    displayName: str = Field(..., max_length=50)
+    password: str = Field(..., max_length=60)
 
 class LoginSchema(BaseModel):
     email: EmailStr
