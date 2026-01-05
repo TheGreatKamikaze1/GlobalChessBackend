55687217b2fe68392b83da8c3d067c05d8b61ef3
The Great Kamikaze <kamikazeweb3@gmail.com>
2026-01-05 21:36:43 +0100

    dd

--- payment_service/app/services/paystack_service.py
+++ payment_service/app/services/paystack_service.py
@@ -4,15 +4,21 @@ import httpx
 from payment_service.app.core.config import settings
 
 
-async def initialize_payment(email: str, amount: int):
-    headers = {
-        "Authorization": f"Bearer {settings.PAYSTACK_SECRET_KEY}",
-        "Content-Type": "application/json",
-    }
+async def initialize_payment(email: str, amount_naira: float):
+    if amount_naira <= 0:
+        raise ValueError("Invalid amount")
+
+    amount_kobo = int(amount_naira * 100)
 
     payload = {
         "email": email,
-        "amount": amount,  # Kobo
+        "amount": amount_kobo,
+        "currency": "NGN",
+    }
+
+    headers = {
+        "Authorization": f"Bearer {settings.PAYSTACK_SECRET_KEY}",
+        "Content-Type": "application/json",
     }
 
     async with httpx.AsyncClient(timeout=30) as client:
@@ -21,9 +27,6 @@ async def initialize_payment(email: str, amount: int):
             json=payload,
             headers=headers,
         )
-
-    if response.status_code != 200:
-        raise Exception(response.text)
 
     return response.json()
 
