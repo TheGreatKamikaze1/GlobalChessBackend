058ad115f888d0f530fcb65aebace2bda9f2b9d1
The Great Kamikaze <kamikazeweb3@gmail.com>
2026-01-05 22:43:17 +0100

    omo

--- payment_service/app/main.py
+++ payment_service/app/main.py
@@ -1,19 +1,21 @@
 from fastapi import FastAPI
-from fastapi import APIRouter
-from payment_service.app.api.routes import paystack, stripe, webhooks
-from payment_service.app.api.routes.paystack import router as payment_router
+
+from payment_service.app.models.payment import Payment  
+
 from payment_service.app.db.base import Base
 from payment_service.app.db.session import engine
 
+from payment_service.app.api.routes import paystack, stripe, webhooks
 
 app = FastAPI(title="Payment Service", version="1.0.0")
 
+
 Base.metadata.create_all(bind=engine)
 
+# ✅ INCLUDE ROUTERS ONCE
 app.include_router(paystack.router)
 app.include_router(stripe.router)
 app.include_router(webhooks.router)
-app.include_router(payment_router)
 
 @app.get("/")
 def root():
