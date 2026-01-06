be30ef697085fbdf921a8f2acbbe0eea637b5cc9
The Great Kamikaze <kamikazeweb3@gmail.com>
2026-01-06 10:12:47 +0100

    users

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from challenges.challenge import router as challenge_router
 from transactions.main import router as transaction_router
 from stats.main import router as stats_router
 from payment_service.app.api.routes.paystack import router as payment_router
+from users.users import router as users_router
 
 
 from sockets.game_socket import game_socket
@@ -58,6 +59,7 @@ app.include_router(auth_router, prefix="/api/auth", tags=["Auth"])
 app.include_router(game_router, prefix="/api/games", tags=["Games"])
 app.include_router(challenge_router, prefix="/api/challenges", tags=["Challenges"])
 app.include_router(transaction_router, prefix="/api/transactions", tags=["Transactions"])
+app.include_router(users_router)
 app.include_router(stats_router, prefix="/api/stats", tags=["Statistics"])
 app.include_router(payment_router)
 
--- users/users.py
+++ users/users.py
@@ -6,7 +6,7 @@ from core.models import User
 from users.users_schema import UpdateProfileSchema
 from core.auth import get_current_user_id
 
-router = APIRouter(prefix="/users", tags=["Users"])
+router = APIRouter( tags=["Users"])
 
 
 
