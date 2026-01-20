8d517c565014a5d3c3397496de19c8f474f3058d
The Great Kamikaze <kamikazeweb3@gmail.com>
2026-01-20 23:09:55 +0100

    F

--- payment_service/app/db/session.py
+++ payment_service/app/db/session.py
@@ -1,9 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
 from payment_service.app.core.config import settings
 
-engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(
+    settings.DATABASE_URL,
+    pool_pre_ping=True,
+    pool_size=10,
+    max_overflow=20,
+    pool_recycle=1800,
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
