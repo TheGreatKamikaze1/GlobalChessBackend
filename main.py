3eadebc8f84575b539341230486ed1de673e2810
The Great Kamikaze <kamikazeweb3@gmail.com>
2026-02-06 17:03:49 +0100

    h

--- main.py
+++ main.py
@@ -32,9 +32,10 @@ def on_startup():
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-
+        "http://localhost:3000",
+        "http://localhost:5173",
         "https://global-connect-chess.netlify.app/",
-        # Add your production frontend origin(s) here
+        
     ],
     allow_methods=["*"],
     allow_headers=["*"],
